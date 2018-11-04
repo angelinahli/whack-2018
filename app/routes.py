1461defc9fee879937384c7aaba4164bc06635ab
Hala Lotfy <Halpha@halpha.local>
2018-11-03 23:20:07 -0400

    HOLY HECK IT DO A WORK

--- app/routes.py
+++ app/routes.py
@@ -50,7 +50,7 @@ def get_next_msg_key(user, txt):
         "CHECKIN_YES":"CHECKIN_BASELINE",
         "CHECKIN_BASELINE":"CHECKIN_RESP",
         "CHECKIN_RESP":"INTERV_START",
-        "INTERV_START":"INVERV_YES",
+        "INTERV_START":"INTERV_YES",
         "INTERV_YES":"INTERV_TYPE",
         "INTERV_TYPE":"INTERV_LENGTH",
         "INTERV_LENGTH":"INTERV_PROMPT",
@@ -60,6 +60,7 @@ def get_next_msg_key(user, txt):
         "INTERV_END":"JOURNAL_START",
         "INTERV_NO":"JOURNAL_START",
         "JOURNAL_YES":"JOURNAL_PROMPT",
+        "JOURNAL_NO":"WRAPUP",
         "JOURNAL_PROMPT":"JOURNAL_END",
         "JOURNAL_END":"WRAPUP"
     }
@@ -67,18 +68,20 @@ def get_next_msg_key(user, txt):
     if prev_key in key_dict:
         return key_dict[prev_key]
     
-    p_head, p_fork = prev_key.split("_")
-    if p_fork == "INTRODUCTION":
+    if prev_key == "INTRODUCTION":
         return "ONBOARDING_START" if not user.has_onboarded else "CHECKIN_START"
     
-    elif p_fork == "START":
-        return p_head + "_NO" if no in txt else p_head + "_YES"
+    split_key = prev_key.split("_")
+    if len(split_key) == 2 and split_key[1] == "START":
+        return split_key[0] + "_NO" if "no" in txt else split_key[0] + "_YES"
         
 def get_checkin_resp(txt):
     error_req = "Please enter a number between 1 & 5 :)"
-    if not txt.isdigit():
+    val = None
+    try:
+        val = int(txt)
+    except ValueError:
         return "Sorry, I didn't understand that! " + error_req
-    val = int(txt)
     if val < 1 or val > 5:
         return "Sorry, I didn't catch that! " + error_req
     if val in [1, 2]:
@@ -98,18 +101,12 @@ def get_interv_prompt(user, txt):
 def handle_prev_resp(user, txt):
     # before we move on, we have to handle the info the user sent us
     # handle cases that require additional processing
-    if msg_key == "ONBOARDING_FINISH":
-        pass
-    elif msg_key == "CHECKIN_BASELINE":
-        pass
-    elif msg_key == "":
-        pass
-        
-def get_next_resp_text(user, txt):
-    msg_key = get_next_msg_key(user, txt)
+    pass
+       
+def get_next_resp_text(user, txt, msg_key):
     with open("conversation.json", "r") as fl:
-        txt = fl.read()
-    convos = json.loads(txt)
+        conv_text = fl.read()
+    convos = json.loads(conv_text)
     
     # grab response text
     resp_txt = None
@@ -118,8 +115,8 @@ def get_next_resp_text(user, txt):
     
     # there are some things we need to personalize
     elif msg_key == "CHECKIN_RESP":
-        return get_checkin_resp(user, txt)
-    elif msg_key == "INTERV_PROMP"
+        return get_checkin_resp(txt)
+    elif msg_key == "INTERV_PROMPT":
         return get_interv_prompt(user, txt)
 
 def handle_post_message(output):
@@ -148,8 +145,16 @@ def handle_post_message(output):
                 db.session.commit()
 
             # figure out what to do next
-            resp_text = get_next_resp_text(user, txt)
+            next_msg_key = get_next_msg_key(user, txt)
+            print("\n\n\nTESTING")
+            print(user.last_action, next_msg_key)
+            print("\n\n\n")
+            resp_text = get_next_resp_text(user, txt, next_msg_key)
             send_message(fb_id, resp_text)
+            
+            user.last_action = next_msg_key
+            db.session.commit()
+            db.session.close()
 
     # this is fine, it's just easier to try except than use for loops 
     except IndexError: 
